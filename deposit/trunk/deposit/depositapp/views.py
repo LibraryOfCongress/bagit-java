d7be4afbddb7ded7c22bdc994b2085a1050a2d7a
Dan Chudnov <dchudnov@rdc.lctl.gov>
2008-12-11 23:13:30 +0000

    Reordering imports by alpha to be easier to scan
    git-svn-id: file:///storage/svn/bagit@1081 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- deposit/trunk/deposit/depositapp/views.py
+++ deposit/trunk/deposit/depositapp/views.py
@@ -1,14 +1,15 @@
-from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseBadRequest, HttpResponseForbidden
 from django.contrib.auth import REDIRECT_FIELD_NAME
-from django.shortcuts import render_to_response
-from django.contrib.auth.views import login as base_login, logout_then_login
-from django.contrib.auth.models import User
-from django.views.generic.create_update import create_object
-import deposit.depositapp.models as models
-import deposit.depositapp.forms as forms
-from django.template import RequestContext
-from django.core.urlresolvers import reverse
 from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.models import User
+from django.contrib.auth.views import login as base_login, logout_then_login
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseBadRequest, HttpResponseForbidden
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.views.generic.create_update import create_object
+
+import deposit.depositapp.forms as forms
+import deposit.depositapp.models as models
 from deposit.depositapp.queries import TransferQuery
 
 
@@ -186,3 +187,4 @@ def list_transfer(request):
     transfers = q.query()
     return render_to_response("transfer_list.html", {'query':q,'transfers':transfers}, 
             context_instance=RequestContext(request))
+
