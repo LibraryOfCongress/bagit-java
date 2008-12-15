f6739aabb1014c29f592b35f2e009a952bd84f9d
Ed Summers <esummers@rdc.lctl.gov>
2008-12-15 20:05:41 +0000

    added configuration files and updated INSTALL
    git-svn-id: file:///storage/svn/bagit@1094 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- deposit/trunk/conf/depositqa-settings.py
+++ deposit/trunk/conf/depositqa-settings.py
@@ -9,10 +9,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/tmp/deposit.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'deposit'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'deposit'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'DATABASE_PASSWORD'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
