380689869f16de3d34a9645b58fd81dcf3042778
Justin Littman <justin@rdc.lctl.gov>
2009-02-23 13:58:50 +0000

    Fixed error on 0.96 bag-info.txt
    git-svn-id: file:///storage/svn/bagit@1235 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagit/trunk/src/test/java/gov/loc/repository/bagit/v0_96/BagImplTest.java
+++ bagit/trunk/src/test/java/gov/loc/repository/bagit/v0_96/BagImplTest.java
@@ -30,4 +30,11 @@ public class BagImplTest {
 		this.getBag("bags/v0_96/bag_with_backslash_filename.tar").isComplete();
 	}
 	
+	@Test
+	public void testBag() throws Exception {
+		Bag bag = this.getBag("bags/v0_96/bag_with_one_manifest");
+		assertEquals("0.96", bag.getBagItTxt().getVersion());
+		assertNotNull(bag.getBagInfoTxt());
+	}
+	
 }
