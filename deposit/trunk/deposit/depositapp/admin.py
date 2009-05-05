4e0fccd56bdd0e2e98485a977f1534c985e35544
Dan Chudnov <dchudnov@rdc.lctl.gov>
2009-05-05 22:06:06 +0000

    Renaming deposit root dir, refs #314
    git-svn-id: file:///storage/svn/bagit@1570 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- deposit/trunk/deposit/depositapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from deposit.depositapp.models import UserProfile, UserProject, \
-    Project, NetworkTransfer, ShipmentTransfer
-
-admin.site.register((UserProfile, UserProject,
-    Project, NetworkTransfer, ShipmentTransfer))
