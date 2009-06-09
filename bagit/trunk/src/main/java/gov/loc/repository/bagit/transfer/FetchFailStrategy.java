aff8ccf2d89c13d3f2ecc0bbfae29529f76e44e8
Brian Vargas <brian@rdc.lctl.gov>
2009-06-09 16:25:53 +0000

    Adding some javadoc to the FetchFailStrategy interface.  No functional changes.
    git-svn-id: file:///storage/svn/bagit@1704 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagit/trunk/src/main/java/gov/loc/repository/bagit/transfer/FetchFailStrategy.java
+++ bagit/trunk/src/main/java/gov/loc/repository/bagit/transfer/FetchFailStrategy.java
@@ -10,8 +10,20 @@ package gov.loc.repository.bagit.transfer;
  * <p>Implementations of this interface <strong>must be
  * thread-safe</strong>.</p>
  * 
+ * <p>Fetch failures are used by the the
+ * {@link BagFetcher#setFetchFailStrategy(FetchFailStrategy) setFetchFailStrategy}
+ * method of the <c>BagFetcher</c>.  Some common strategies are
+ * the {@link StandardFailStrategies#ALWAYS_CONTINUE ALWAYS_CONTINUE},
+ * {@link StandardFailStrategies#ALWAYS_RETRY ALWAYS_RETRY},
+ * and {@link StandardFailStrategies#FAIL_FAST FAIL_FAST} strategies.
+ * A more complex strategy might involve the
+ * {@link ThresholdFailStrategy}.</p>
+ * 
  * @version $id$
  * @see FetchFailureAction
+ * @see StandardFailStrategies
+ * @see ThresholdFailStrategy
+ * @see BagFetcher
  */
 public interface FetchFailStrategy
 {
