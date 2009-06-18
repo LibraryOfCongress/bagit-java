c272bd22faa74430d49a2a9e5b83b9ad7631e104
Justin Littman <justin@rdc.lctl.gov>
2009-06-18 12:52:21 +0000

    closes #434
    git-svn-id: file:///storage/svn/bagit@1783 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagit/trunk/src/main/java/gov/loc/repository/bagit/writer/impl/FileSystemWriter.java
+++ bagit/trunk/src/main/java/gov/loc/repository/bagit/writer/impl/FileSystemWriter.java
@@ -2,6 +2,7 @@ package gov.loc.repository.bagit.writer.impl;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.text.MessageFormat;
 
@@ -15,6 +16,7 @@ import gov.loc.repository.bagit.BagFile;
 import gov.loc.repository.bagit.Bag.Format;
 import gov.loc.repository.bagit.BagFactory.LoadOption;
 import gov.loc.repository.bagit.impl.VFSBagFile;
+import gov.loc.repository.bagit.utilities.FilenameHelper;
 import gov.loc.repository.bagit.utilities.VFSHelper;
 
 public class FileSystemWriter extends AbstractWriter {
@@ -116,9 +118,49 @@ public class FileSystemWriter extends AbstractWriter {
 		log.info("Writing bag");
 		this.newBagDir = file;
 		bag.accept(this);
+		if (this.newBagDir.equals(bag.getFile())) {
+			log.debug("Removing any extra files or directories");
+			this.removeExtraFiles(this.newBagDir);
+		}
 		if (this.isCancelled()) return null;
 		return this.newBag;		
 
+	}
+	
+	private void removeExtraFiles(File dir) {
+		log.trace(MessageFormat.format("Checking children of {0} for removal", dir));
+		for(File file : dir.listFiles()) {
+			if (this.isCancelled()) return;
+			if (file.isDirectory()) {
+				if (log.isTraceEnabled()) {
+					log.trace(MessageFormat.format("{0} is a directory with {1} children", file, file.listFiles().length));
+				}
+				this.removeExtraFiles(file);
+				if (log.isTraceEnabled()) {
+					log.trace(MessageFormat.format("{0} now has {1} children", file, file.listFiles().length));
+				}				
+				if (file.listFiles().length == 0) {					
+					try {
+						log.trace("Deleting " + file);
+						FileUtils.forceDelete(file);
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				}
+			} else {
+				String filepath = FilenameHelper.removeBasePath(this.newBagDir.toString(), file.toString());
+				log.trace(MessageFormat.format("{0} is a file whose filepath is {1}", file, filepath));
+				if (this.newBag.getBagFile(filepath) == null) {
+					try {
+						log.trace("Deleting " + file);
+						FileUtils.forceDelete(file);
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}					
+				}				
+			}
+		}
+		
 	}
 	
 }
