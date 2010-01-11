748e49301799cee91a1d65aff42f1acbc3782d23
Praveen Bokka <pbok@rdc.lctl.gov>
2010-01-11 20:47:35 +0000

    refs # 863
    git-svn-id: file:///storage/svn/bagit@2700 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagit/trunk/src/test/java/gov/loc/repository/bagit/driver/CommandLineBagDriverTest.java
+++ bagit/trunk/src/test/java/gov/loc/repository/bagit/driver/CommandLineBagDriverTest.java
@@ -114,6 +114,21 @@ public class CommandLineBagDriverTest {
 	}
 	
 	@Test
+	public void testCreateIncludeDelimiter() throws Exception {
+        assertEquals(RETURN_SUCCESS, driver.execute(new String[] {OPERATION_CREATE, destFile.getAbsolutePath(), ResourceHelper.getFile("bags/v0_95/bag/data/dir1").getAbsolutePath(), ResourceHelper.getFile("bags/v0_95/bag/data/dir2").getAbsolutePath(), "--" + PARAM_WRITER, VALUE_WRITER_ZIP, "--" + PARAM_MANIFEST_DELIMITER, "    \t\t" }));
+        Bag bag = this.bagFactory.createBag(destFile);
+        assertEquals(3, bag.getPayload().size());
+        assertTrue(bag.verifyValid().isSuccess());
+        BagInfoTxt bagInfo = bag.getBagInfoTxt();
+        assertNotNull(bagInfo);
+        assertNotNull(bagInfo.getBaggingDate());
+        assertNotNull(bagInfo.getBagSize());
+        assertNotNull(bagInfo.getPayloadOxum());
+        assertEquals(1, bag.getTagManifests().size());
+	}
+
+	
+	@Test
 	public void testMakeHoley() throws Exception {
 		final String BASE_URL = "http://foo.com/bag";
         assertEquals(RETURN_SUCCESS, driver.execute(new String[] {OPERATION_MAKE_HOLEY, ResourceHelper.getFile("bags/v0_95/bag").getAbsolutePath(), destFile.getAbsolutePath(), BASE_URL, "--" + PARAM_WRITER, VALUE_WRITER_ZIP}));
@@ -124,4 +139,45 @@ public class CommandLineBagDriverTest {
         assertTrue(fetch.get(0).getUrl().startsWith(BASE_URL));
 	}
 	
+//	@Test
+//	public void testMakeHoleyIncludeDelimiter() throws Exception {
+//		final String BASE_URL = "http://foo.com/bag";
+//        assertEquals(RETURN_SUCCESS, driver.execute(new String[] {OPERATION_MAKE_HOLEY, ResourceHelper.getFile("bags/v0_96/bag").getAbsolutePath(), destFile.getAbsolutePath(), BASE_URL, "--" + PARAM_WRITER, VALUE_WRITER_ZIP, "--" + PARAM_MANIFEST_DELIMITER, "    \t\t" }));
+//        assertTrue(destFile.exists());
+//        Bag bag = this.bagFactory.createBag(destFile);
+//        FetchTxt fetch = bag.getFetchTxt();
+//        assertNotNull(fetch);
+//        assertTrue(fetch.get(0).getUrl().startsWith(BASE_URL));
+//	}
+	
+	@Test
+	public void testUpdateIncludeDelimiter() throws Exception {
+	    assertEquals(RETURN_SUCCESS, driver.execute(new String[] {OPERATION_CREATE, destFile.getAbsolutePath(), ResourceHelper.getFile("bags/v0_95/bag/data/dir1").getAbsolutePath(), ResourceHelper.getFile("bags/v0_95/bag/data/dir2").getAbsolutePath()}));
+	    assertEquals(RETURN_SUCCESS, driver.execute(new String[] {OPERATION_UPDATE, destFile.getAbsolutePath(), "--" + PARAM_MANIFEST_DELIMITER, "    \t\t" }));
+        Bag bag = this.bagFactory.createBag(destFile);
+        assertEquals(3, bag.getPayload().size());
+        assertTrue(bag.verifyValid().isSuccess());
+        BagInfoTxt bagInfo = bag.getBagInfoTxt();
+        assertNotNull(bagInfo);
+        assertNotNull(bagInfo.getBaggingDate());
+        assertNotNull(bagInfo.getBagSize());
+        assertNotNull(bagInfo.getPayloadOxum());
+        assertEquals(1, bag.getTagManifests().size());
+	}
+
+//	@Test
+//	public void testUpdateTagManifestsIncludeDelimiter() throws Exception {
+//	    assertEquals(RETURN_SUCCESS, driver.execute(new String[] {OPERATION_CREATE, destFile.getAbsolutePath(), ResourceHelper.getFile("bags/v0_95/bag/data/dir1").getAbsolutePath(), ResourceHelper.getFile("bags/v0_95/bag/data/dir2").getAbsolutePath()}));
+//	    assertEquals(RETURN_SUCCESS, driver.execute(new String[] {OPERATION_UPDATE_TAGMANIFESTS, destFile.getAbsolutePath(), "--" + PARAM_MANIFEST_DELIMITER, "\t" }));
+//        Bag bag = this.bagFactory.createBag(destFile);
+//        assertEquals(3, bag.getPayload().size());
+//        assertTrue(bag.verifyValid().isSuccess());
+//        BagInfoTxt bagInfo = bag.getBagInfoTxt();
+//        assertNotNull(bagInfo);
+//        assertNotNull(bagInfo.getBaggingDate());
+//        assertNotNull(bagInfo.getBagSize());
+//        assertNotNull(bagInfo.getPayloadOxum());
+//        assertEquals(1, bag.getTagManifests().size());
+//	}
+
 }
