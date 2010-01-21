604badc94a60f29fd8d2d3a129994c87e3b5d9e4
Brian Vargas <brian@rdc.lctl.gov>
2010-01-21 17:56:02 +0000

    Refactoring AbstractBagImplTest to provide protected hook points for overriding test classes to change up assertions in non-standard situations. refs #920
    git-svn-id: file:///storage/svn/bagit@2768 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagit/trunk/src/test/java/gov/loc/repository/bagit/impl/AbstractBagImplTest.java
+++ bagit/trunk/src/test/java/gov/loc/repository/bagit/impl/AbstractBagImplTest.java
@@ -95,40 +95,85 @@ public abstract class AbstractBagImplTest {
 		return testBagDir;
 	}
 	
-	private void testBag(Bag bag) throws Exception {
-		List<Manifest> payloadManifests = bag.getPayloadManifests();
-		assertEquals(1, payloadManifests.size());
-		assertEquals("manifest-md5.txt", payloadManifests.get(0).getFilepath());
-		assertEquals("manifest-md5.txt", bag.getPayloadManifest(Algorithm.MD5).getFilepath());
-		assertNull(bag.getPayloadManifest(Algorithm.SHA1));
-		assertEquals("ad0234829205b9033196ba818f7a872b", bag.getChecksums("data/test2.txt").get(Algorithm.MD5));
-		
+	protected void testBag(Bag bag) throws Exception
+	{
+		this.assertBagPayloadManifests(bag);
+		this.assertBagTagManifests(bag);
+		this.assertBagTagFiles(bag);
+		this.assertBagPayloadFiles(bag);		
+		this.assertBagDeclaration(bag);		
+		this.assertBagInfo(bag);
+		this.assertBagComplete(bag);
+		this.assertBagValid(bag);
+		this.assertBagVerifyTagManifests(bag);
+		this.assertBagVerifyPayloadManifests(bag);
+	}
+
+	protected void assertBagVerifyPayloadManifests(Bag bag) throws Exception
+	{
+		assertTrue("Verify Payload Manifests", bag.verifyPayloadManifests().isSuccess());
+	}
+
+	protected void assertBagVerifyTagManifests(Bag bag) throws Exception
+	{
+		assertTrue("Verify Tag Manifests", bag.verifyTagManifests().isSuccess());
+	}
+
+	protected void assertBagValid(Bag bag) throws Exception
+	{
+		assertTrue("Verify Valid", bag.verifyValid().isSuccess());
+	}
+
+	protected void assertBagComplete(Bag bag) throws Exception
+	{
+		assertTrue("Verify Complete", bag.verifyComplete().isSuccess());
+	}
+
+	protected void assertBagInfo(Bag bag) throws Exception
+	{
+		BagInfoTxt bagInfo = bag.getBagInfoTxt();
+		assertEquals("Spengler University", bagInfo.getSourceOrganization());
+	}
+
+	protected void assertBagDeclaration(Bag bag) throws Exception
+	{
+		BagItTxt bagIt = bag.getBagItTxt();
+		assertEquals("UTF-8", bagIt.getCharacterEncoding());
+		assertEquals(this.getVersion().versionString, bagIt.getVersion());
+	}
+
+	private void assertBagPayloadFiles(Bag bag) throws Exception
+	{
+		assertEquals(5, bag.getPayload().size());
+		assertNotNull(bag.getBagFile("data/dir1/test3.txt"));
+		assertNull(bag.getBagFile("xdata/dir1/test3.txt"));
+	}
+
+	protected void assertBagTagFiles(Bag bag) throws Exception
+	{
+		assertEquals(4, bag.getTags().size());
+		assertNotNull(bag.getBagFile("bagit.txt"));
+		assertNull(bag.getBagFile("xbagit.txt"));
+	}
+
+	protected void assertBagTagManifests(Bag bag) throws Exception
+	{
 		List<Manifest> tagManifests = bag.getTagManifests();
 		assertEquals(1, tagManifests.size());
 		assertEquals("tagmanifest-md5.txt", tagManifests.get(0).getFilepath());
 		assertEquals("tagmanifest-md5.txt", bag.getTagManifest(Algorithm.MD5).getFilepath());
 		assertNull(bag.getTagManifest(Algorithm.SHA1));
 		assertNotNull(bag.getChecksums("bagit.txt").get(Algorithm.MD5));
-		
-		assertEquals(4, bag.getTags().size());
-		assertNotNull(bag.getBagFile("bagit.txt"));
-		assertNull(bag.getBagFile("xbagit.txt"));
-				
-		assertEquals(5, bag.getPayload().size());
-		assertNotNull(bag.getBagFile("data/dir1/test3.txt"));
-		assertNull(bag.getBagFile("xdata/dir1/test3.txt"));		
-		
-		BagItTxt bagIt = bag.getBagItTxt();
-		assertEquals("UTF-8", bagIt.getCharacterEncoding());
-		assertEquals(this.getVersion().versionString, bagIt.getVersion());
-		
-		BagInfoTxt bagInfo = bag.getBagInfoTxt();
-		assertEquals("Spengler University", bagInfo.getSourceOrganization());
+	}
 
-		assertTrue("Verify Complete", bag.verifyComplete().isSuccess());
-		assertTrue("Verify Valid", bag.verifyValid().isSuccess());
-		assertTrue("Verify Tag Manifests", bag.verifyTagManifests().isSuccess());
-		assertTrue("Verify Payload Manifests", bag.verifyPayloadManifests().isSuccess());
+	protected void assertBagPayloadManifests(Bag bag) throws Exception
+	{
+		List<Manifest> payloadManifests = bag.getPayloadManifests();
+		assertEquals(1, payloadManifests.size());
+		assertEquals("manifest-md5.txt", payloadManifests.get(0).getFilepath());
+		assertEquals("manifest-md5.txt", bag.getPayloadManifest(Algorithm.MD5).getFilepath());
+		assertNull(bag.getPayloadManifest(Algorithm.SHA1));
+		assertEquals("ad0234829205b9033196ba818f7a872b", bag.getChecksums("data/test2.txt").get(Algorithm.MD5));
 	}
 	
 	@Test
