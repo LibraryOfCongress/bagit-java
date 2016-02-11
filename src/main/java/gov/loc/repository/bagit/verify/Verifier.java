8a9a6ef12c4ced9a256f2f254089550679fef37f
John Scancella <jsca@loc.gov>
2016-02-11 15:03:09 -0500

    removed some javadoc generation warnings

--- src/main/java/gov/loc/repository/bagit/verify/Verifier.java
+++ src/main/java/gov/loc/repository/bagit/verify/Verifier.java
@@ -46,7 +46,7 @@ public class Verifier {
   }
 
   /**
-   *  See <a href="https://tools.ietf.org/html/draft-kunze-bagit-13#section-3">https://tools.ietf.org/html/draft-kunze-bagit-13#section-3</a></br>
+   *  See <a href="https://tools.ietf.org/html/draft-kunze-bagit-13#section-3">https://tools.ietf.org/html/draft-kunze-bagit-13#section-3</a><br>
    *  A bag is <b>valid</b> if the bag is complete and every checksum has been 
    *  verified against the contents of its corresponding file.
    * @throws IOException 
@@ -96,15 +96,15 @@ public class Verifier {
   }
   
   /**
-   * See <a href="https://tools.ietf.org/html/draft-kunze-bagit-13#section-3">https://tools.ietf.org/html/draft-kunze-bagit-13#section-3</a></br>
-   * A bag is <b>complete</b> if </br>
-   * <p><ul>
+   * See <a href="https://tools.ietf.org/html/draft-kunze-bagit-13#section-3">https://tools.ietf.org/html/draft-kunze-bagit-13#section-3</a><br>
+   * A bag is <b>complete</b> if <br>
+   * <ul>
    * <li>every element is present
    * <li>every file in the payload manifest(s) are present
    * <li>every file in the tag manifest(s) are present. Tag files not listed in a tag manifest may be present.
    * <li>every file in the data directory must be listed in at least one payload manifest
    * <li>each element must comply with the bagit spec
-   * </ul></p>
+   * </ul>
    * @throws IOException if there was an error with the file
    * @throws MissingPayloadManifestException if there is not at least one payload manifest
    * @throws MissingBagitFileException  if there is no bagit.txt file
