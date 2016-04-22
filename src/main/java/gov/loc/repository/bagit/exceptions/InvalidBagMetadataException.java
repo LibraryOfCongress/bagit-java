1937ad65e4116a0c001ceb61e082161a29498ca1
John Scancella <jsca@loc.gov>
2016-04-22 13:45:13 -0400

    fixing javadoc

--- src/main/java/gov/loc/repository/bagit/exceptions/InvalidBagMetadataException.java
+++ src/main/java/gov/loc/repository/bagit/exceptions/InvalidBagMetadataException.java
@@ -2,10 +2,11 @@ package gov.loc.repository.bagit.exceptions;
 
 /**
  * Class to represent an error when the bag metadata file does not conform to the bagit spec, 
- * namely: </br> < KEY >:< VALUE > 
- * </br>or</br>
- * < KEY >:< VALUE ></br>
- * < INDENT >< VALUE CONTINUED >
+ * namely: <br>
+ * &lt;KEY&gt;:&lt;VALUE&gt; 
+ * <br>or
+ * <pre>&lt;KEY&gt;:&lt;VALUE&gt;
+ *    &lt;VALUE CONTINUED&gt;</pre>
  */
 public class InvalidBagMetadataException extends Exception {
   private static final long serialVersionUID = 1L;
