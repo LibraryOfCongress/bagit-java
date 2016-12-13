1629a437801b053d0dd1d8b43e455e67a314da4a
John Scancella <jsca@loc.gov>
2016-12-13 14:55:35 -0500

    refs #61 - added a convenience method for checking for all bag warnings in the bag linter

--- src/main/java/gov/loc/repository/bagit/conformance/BagLinter.java
+++ src/main/java/gov/loc/repository/bagit/conformance/BagLinter.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.Normalizer;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -46,6 +47,26 @@ public class BagLinter {
   
   public BagLinter(final BagitAlgorithmNameToSupportedAlgorithmMapping nameMapping){
     reader = new BagReader(nameMapping);
+  }
+  
+  /**
+   * The BagIt specification is very flexible in what it allows which leads to situations 
+   * where something may be technically allowed, but should be discouraged.
+   * This method checks a bag for potential problems, or other items that are allowed but discouraged.
+   * This <strong>does not</strong> validate a bag. See {@link BagVerifier} instead.
+   * 
+   * @param rootDir the root directory of the bag
+   * 
+   * @return a set of {@link BagitWarning} detailing all items that should be fixed.
+   * 
+   * @throws InvalidBagMetadataException if the bag metadata does not conform to the bagit specification
+   * @throws UnparsableVersionException if there is an error reading the bagit version
+   * @throws IOException if there was an error reading a file
+   * @throws UnsupportedAlgorithmException if the {@link BagReader} does not support reading the manifest 
+   * @throws MaliciousPathException If the manifest was crafted to try and access a file outside the bag directory
+   */
+  public Set<BagitWarning> lintBag(final Path rootDir) throws IOException, UnparsableVersionException, InvalidBagMetadataException, MaliciousPathException, UnsupportedAlgorithmException{
+    return this.lintBag(rootDir, Collections.emptyList());
   }
   
   /**
