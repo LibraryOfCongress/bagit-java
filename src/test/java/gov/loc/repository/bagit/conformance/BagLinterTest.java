6df2a5d5d0cd3f08a3fa71a2f1cc9bad9cbe8c9b
John Scancella <jsca@loc.gov>
2017-02-13 09:27:34 -0500

    more code coverage

--- src/test/java/gov/loc/repository/bagit/conformance/BagLinterTest.java
+++ src/test/java/gov/loc/repository/bagit/conformance/BagLinterTest.java
@@ -1,13 +1,19 @@
 package gov.loc.repository.bagit.conformance;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import gov.loc.repository.bagit.domain.Bag;
 
 public class BagLinterTest extends Assert{
   
@@ -28,5 +34,16 @@ public class BagLinterTest extends Assert{
     diff.removeAll(warnings);
     
     assertEquals("Warnings missing: " + diff.toString() + "\n", expectedWarnings, warnings);
+  }
+  
+  @Test
+  public void testCheckAgainstProfile() throws Exception{
+    Path profileJson = new File("src/test/resources/bagitProfiles/exampleProfile.json").toPath();
+    Path bagRootPath = new File("src/test/resources/bagitProfileTestBags/profileConformantBag").toPath();
+    Bag bag = sut.getReader().read(bagRootPath);
+    
+    try(InputStream inputStream = Files.newInputStream(profileJson, StandardOpenOption.READ)){
+      sut.checkAgainstProfile(inputStream, bag);
+    }
   }
 }
