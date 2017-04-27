e2e2a7004c3eddd86085535e0ab39e1f92219c66
John Scancella <jsca@loc.gov>
2017-04-27 07:48:28 -0400

    increase code coverage by testing private constructor

--- src/test/java/gov/loc/repository/bagit/reader/KeyValueReaderTest.java
+++ src/test/java/gov/loc/repository/bagit/reader/KeyValueReaderTest.java
@@ -1,15 +1,21 @@
 package gov.loc.repository.bagit.reader;
 
+import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import gov.loc.repository.bagit.PrivateConstructorTest;
 import gov.loc.repository.bagit.exceptions.InvalidBagMetadataException;
 
-public class KeyValueReaderTest extends Assert {
+public class KeyValueReaderTest extends PrivateConstructorTest {
+  
+  @Test
+  public void testClassIsWellDefined() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException{
+    assertUtilityClassWellDefined(KeyValueReader.class);
+  }
 
   @Test(expected=InvalidBagMetadataException.class)
   public void testReadInproperIndentedBagMetadataFileThrowsException() throws Exception{
