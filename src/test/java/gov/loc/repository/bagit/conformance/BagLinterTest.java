4d0834d9e1f6c4d2ad5ccd63735be8a416b23555
Thomas Jejkal <thomas.jejkal@kit.edu>
2018-01-16 07:30:37 +0100

    Fixed bagDataDir argument in PayloadWriterTest#testWritePayloadFilesMinusFetchFiles. Disabled removal of BagitWarning.DIFFERENT_NORMALIZATION in BagLinterTest as it seems to be produced also under MacOS in the meantime (resolves #102) Fixed PayloadWriter#getFetchPaths to resolve fetch file path against the bag data folder correctly (resolves #99)

--- src/test/java/gov/loc/repository/bagit/conformance/BagLinterTest.java
+++ src/test/java/gov/loc/repository/bagit/conformance/BagLinterTest.java
@@ -3,7 +3,6 @@ package gov.loc.repository.bagit.conformance;
 import java.io.File;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,45 +18,45 @@ import gov.loc.repository.bagit.domain.Bag;
 import gov.loc.repository.bagit.reader.BagReader;
 
 public class BagLinterTest extends PrivateConstructorTest{
-  
-  private final Path rootDir = Paths.get("src","test","resources","linterTestBag");
-  
-  @Test
-  public void testClassIsWellDefined() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException{
-    assertUtilityClassWellDefined(BagLinter.class);
-  }
-  
-  @Test
-  public void testLintBag() throws Exception{
-    Set<BagitWarning> expectedWarnings = new HashSet<>();
-    expectedWarnings.addAll(Arrays.asList(BagitWarning.values()));
-    Set<BagitWarning> warnings = BagLinter.lintBag(rootDir);
 
-    if(FileSystems.getDefault().getClass().getName() == "sun.nio.fs.MacOSXFileSystem"){
-      expectedWarnings.remove(BagitWarning.DIFFERENT_NORMALIZATION); //don't test normalization on mac
+  private final Path rootDir = Paths.get("src","test","resources","linterTestBag");
+
+    @Test
+  public void testClassIsWellDefined() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException{
+        assertUtilityClassWellDefined(BagLinter.class);
     }
-    
-    Set<BagitWarning> diff = new HashSet<>(expectedWarnings);
-    diff.removeAll(warnings);
-    
-    assertEquals("Warnings missing: " + diff.toString() + "\n", expectedWarnings, warnings);
-  }
-  
-  @Test
+
+    @Test
+  public void testLintBag() throws Exception{
+        Set<BagitWarning> expectedWarnings = new HashSet<>();
+        expectedWarnings.addAll(Arrays.asList(BagitWarning.values()));
+        Set<BagitWarning> warnings = BagLinter.lintBag(rootDir);
+        
+        /*if (FileSystems.getDefault().getClass().getName() == "sun.nio.fs.MacOSXFileSystem"){
+            expectedWarnings.remove(BagitWarning.DIFFERENT_NORMALIZATION); //don't test normalization on mac
+        }*/
+
+        Set<BagitWarning> diff = new HashSet<>(expectedWarnings);
+        diff.removeAll(warnings);
+
+        assertEquals("Warnings missing: " + diff.toString() + "\n", expectedWarnings, warnings);
+    }
+
+    @Test
   public void testCheckAgainstProfile() throws Exception{
-    Path profileJson = new File("src/test/resources/bagitProfiles/exampleProfile.json").toPath();
-    Path bagRootPath = new File("src/test/resources/bagitProfileTestBags/profileConformantBag").toPath();
-    BagReader reader = new BagReader();
-    Bag bag = reader.read(bagRootPath);
-    
+        Path profileJson = new File("src/test/resources/bagitProfiles/exampleProfile.json").toPath();
+        Path bagRootPath = new File("src/test/resources/bagitProfileTestBags/profileConformantBag").toPath();
+        BagReader reader = new BagReader();
+        Bag bag = reader.read(bagRootPath);
+
     try(InputStream inputStream = Files.newInputStream(profileJson, StandardOpenOption.READ)){
-      BagLinter.checkAgainstProfile(inputStream, bag);
+            BagLinter.checkAgainstProfile(inputStream, bag);
+        }
     }
-  }
-  
-  @Test
+
+    @Test
   public void testIgnoreCheckForExtraLines() throws Exception{
-    Set<BagitWarning> warnings = BagLinter.lintBag(rootDir, Arrays.asList(BagitWarning.EXTRA_LINES_IN_BAGIT_FILES));
-    assertFalse(warnings.contains(BagitWarning.EXTRA_LINES_IN_BAGIT_FILES));
-  }
+        Set<BagitWarning> warnings = BagLinter.lintBag(rootDir, Arrays.asList(BagitWarning.EXTRA_LINES_IN_BAGIT_FILES));
+        assertFalse(warnings.contains(BagitWarning.EXTRA_LINES_IN_BAGIT_FILES));
+    }
 }
